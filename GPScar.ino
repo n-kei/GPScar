5110f73d214493030391f575a087fa59e56a7432
sato ryouta <kopatroopers@gmail.com>
2015-11-19 18:42:11 +0900

    気圧センサ動いた

--- GPScar.ino
+++ GPScar.ino
@@ -2,13 +2,15 @@
 #include "Control.h"
 #include "Motor.h"
 #include "run.h"
-
+#include "ADXL345.h"
+#include "skLPSxxSPI.h"
+#include <SPI.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 
 //#define MODE_NOMODIFY //GPSで方位が得られなかったときニュートラルに戻す
-#define MODE_RUN      //自律走行実行モード
-//#define MODE_TEST   //テストモード
+//#define MODE_RUN      //自律走行実行モード
+#define MODE_TEST   //テストモード
 
 #define STACKCOUNTER_NUM 10
 
@@ -20,11 +22,11 @@
 ////////////////////////////////////////
 
 ///////////各種定数///////////////
-#define DEF_WR_M 2.0   //ウェイポイントの半径[m]
-#define DEF_KS   0.1    //ステアリングキレ角比例係数
-#define DEF_STRMIN 30     //ステアリング最小キレ角[duty]
-#define DEF_STRMAX 255    //ステアリング最大キレ角[duty]
-#define DEF_RUNSP  125   //ステアリングニュートラルの時のモータ[duty]
+#define DEF_WR_M    2.0      //ウェイポイントの半径[m]
+#define DEF_KS      0.1      //ステアリングキレ角比例係数
+#define DEF_STRMIN  30     //ステアリング最小キレ角[duty]
+#define DEF_STRMAX  255    //ステアリング最大キレ角[duty]
+#define DEF_RUNSP   125    //ステアリングニュートラルの時のモータ[duty]
 //////////////////////////////////
 
 /////////座標関係/////////////////
@@ -56,7 +58,7 @@ void setup()
   setOriginPoint(gps.location.lat(), gps.location.lng());
 
   motor.control(255, 255);
-  delay(10000);
+  delay(30000);
   motor.control(0, 0);
 }
 
@@ -75,7 +77,7 @@ void loop()
     motor.control(0, 0);
     return;
   }
-  
+
   posSteer = control.getPID(currentDir_deg, 0);
 
   motor.control(DEF_RUNSP - posSteer, DEF_RUNSP + posSteer);
@@ -98,24 +100,47 @@ void loop()
 
 #ifdef MODE_TEST
 
-TinyGPSPlus gps;
-struct SerialPeripheral ss = {SOFTWARESERIAL, 9600, 10, 9};
-ReceiveGPS recvGPS(&gps, ss);
+skLPSxxx LPS(LPS331AP, SS) ;    // デバイスはLPS25Hを指定、SS=10
+
+float pressure_origin;
 
 void setup()
 {
-  Serial.begin(9600);
+  int ans ;
+
+  // シリアルモニターの設定
+  Serial.begin(9600) ;
+  // ＳＰＩの初期化
+  SPI.begin() ;                           // ＳＰＩを行う為の初期化
+  SPI.setBitOrder(MSBFIRST) ;             // ビットオーダー
+  SPI.setClockDivider(SPI_CLOCK_DIV4) ;   // クロック(CLK)をシステムクロックの1/4で使用(16MHz/4)
+  SPI.setDataMode(SPI_MODE3) ;            // クロック極性 1(HIGH)　クロック位相 1(HIGH)
+
+  delay(3000) ;                           // 3Sしたら開始
+  // 気圧センサの初期化を行う(更新速度は1Hz)
+  ans = LPS.PressureInit() ;
+  if (ans == 0) Serial.println("Initialization normal") ;
+  else {
+    Serial.print("Initialization abnormal ans=") ;
+    Serial.println(ans) ;
+  }
+  delay(1000) ;
+  LPS.PressureRead();
+  pressure_origin = LPS.getPressure();
+  Serial.println(pressure_origin);
 }
 
 void loop()
 {
-  double angle;
-
-  angle = TinyGPSPlus::courseTo(35.6544, 139.74477, 21.4225, 39.8261);
-  Serial.print("angle = ");  Serial.println(changeRange_deg(angle));
-  delay(1000);
-
+  LPS.PressureRead() ;               // 圧力と温度を読み出す
+  Serial.print("[LPS331AP]") ;
+  Serial.print(LPS.getPressure()) ;              // 気圧値の表示を行う
+  Serial.print(" hPa  ") ;
+  Serial.print(LPS.getTempreture()) ;               // 温度の表示を行う
+  Serial.print(" 'C ") ;
+  Serial.print(LPS.AltitudeCalc(pressure_origin, LPS.getPressure()));
+  Serial.println(" m");
+  delay(1000) ;                      // １秒後に繰り返す
 }
-
 #endif
 
